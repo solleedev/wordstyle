541a9700c09e6bb6356732c13e2ed4276973537d
Sollee <solleedev@proton.me>
2023-12-27 15:50:48 +0000

    chore: add yarn to flake

--- flake.nix
+++ flake.nix
@@ -5,33 +5,35 @@
     systems.url = "github:nix-systems/default";
   };
 
-  outputs = {
-    systems,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    eachSystem = f:
-      nixpkgs.lib.genAttrs (import systems) (
-        system:
+  outputs =
+    { systems
+    , nixpkgs
+    , ...
+    } @ inputs:
+    let
+      eachSystem = f:
+        nixpkgs.lib.genAttrs (import systems) (
+          system:
           f nixpkgs.legacyPackages.${system}
-      );
-  in {
-    devShells = eachSystem (pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.nodejs
-          # You can set the major version of Node.js to a specific one instead
-          # of the default version
-          # pkgs.nodejs-19_x
+        );
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nodejs
+            # You can set the major version of Node.js to a specific one instead
+            # of the default version
+            # pkgs.nodejs-19_x
 
-          # You can choose pnpm, yarn, or none (npm).
-          pkgs.nodePackages.pnpm
-          # pkgs.yarn
+            # You can choose pnpm, yarn, or none (npm).
+            pkgs.nodePackages.pnpm
+            pkgs.yarn
 
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.typescript-language-server
-        ];
-      };
-    });
-  };
+            pkgs.nodePackages.typescript
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
 }
